dbe0d4cef3c2d976df1820df0334906b191d704a
AbhiCN <93698894+nowucme2@users.noreply.github.com>
2026-03-11 18:49:15 +0530

    Update report.py

--- module/report.py
+++ module/report.py
@@ -1,18 +1,44 @@
-def generate_report(subnets, project_dir):
+import json
+from pathlib import Path
 
-    report = f"{project_dir}/report.html"
+def generate_html_report(httpx_json, report_file):
 
-    with open(report, "w") as f:
+    rows = []
 
-        f.write("<html><body>")
-        f.write("<h1>ASN Breaker Report</h1>")
+    with open(httpx_json) as f:
+        for line in f:
+            data = json.loads(line)
 
-        f.write("<h2>Subnets</h2><ul>")
+            url = data.get("url","")
+            title = data.get("title","")
+            status = data.get("status-code","")
+            tech = ",".join(data.get("tech",[]))
 
-        for s in subnets:
+            rows.append(f"""
+            <tr>
+            <td>{url}</td>
+            <td>{status}</td>
+            <td>{title}</td>
+            <td>{tech}</td>
+            </tr>
+            """)
 
-            f.write(f"<li>{s['subnet']}</li>")
+    html = f"""
+    <html>
+    <body>
+    <h1>ASN Breaker Report</h1>
+    <table border="1">
+    <tr>
+    <th>URL</th>
+    <th>Status</th>
+    <th>Title</th>
+    <th>Technology</th>
+    </tr>
+    {''.join(rows)}
+    </table>
+    </body>
+    </html>
+    """
 
-        f.write("</ul></body></html>")
-
-    print(f"\nReport generated: {report}")
+    with open(report_file,"w") as f:
+        f.write(html)
