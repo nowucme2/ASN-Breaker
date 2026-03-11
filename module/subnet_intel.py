28cd473de2de2c27e58b8d2e2f5f28137c599a18
AbhiCN <93698894+nowucme2@users.noreply.github.com>
2026-03-11 18:48:24 +0530

    Update subnet_intel.py

--- module/subnet_intel.py
+++ module/subnet_intel.py
@@ -1,24 +1,50 @@
 import ipaddress
+import random
+
+CDN_KEYWORDS = [
+    "cloudflare",
+    "akamai",
+    "fastly"
+]
+
+def clean_subnets(subnets):
+    valid = []
+
+    for s in subnets:
+        try:
+            net = ipaddress.ip_network(s, strict=False)
+
+            if net.version == 6:
+                continue
+
+            if str(net.network_address) == "0.0.0.0":
+                continue
+
+            valid.append(net)
+
+        except:
+            pass
+
+    collapsed = list(ipaddress.collapse_addresses(valid))
+    return collapsed
 
 
-def analyze_subnets(raw):
+def sample_ips(networks):
 
-    result = []
+    final_ips = []
 
-    for entry in raw:
+    for net in networks:
 
-        subnet = entry["subnet"]
-        limit = entry["limit"]
+        size = net.num_addresses
 
-        network = ipaddress.IPv4Network(subnet)
+        if size < 2000:
+            for ip in net.hosts():
+                final_ips.append(str(ip))
+        else:
+            sample_size = 500
+            for _ in range(sample_size):
+                offset = random.randint(1, size-2)
+                ip = net.network_address + offset
+                final_ips.append(str(ip))
 
-        size = network.num_addresses
-
-        print(f"{subnet} | {size} IPs")
-
-        result.append({
-            "subnet": subnet,
-            "limit": limit
-        })
-
-    return result
+    return list(set(final_ips))
