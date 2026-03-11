8570582aeed51f9e84b9911c435822c896816612
AbhiCN <93698894+nowucme2@users.noreply.github.com>
2026-03-11 19:30:21 +0530

    Update web_scan.py

--- module/web_scan.py
+++ module/web_scan.py
@@ -32,15 +32,9 @@ def run_httpx(input_ports, output_json):
 def run_gowitness(url_file, output_dir):
 
     output_dir = Path(output_dir)
-
-    db_path = output_dir / "gowitness.db"
     screenshot_dir = output_dir / "screenshots"
 
     screenshot_dir.mkdir(parents=True, exist_ok=True)
-
-    if db_path.exists() and db_path.stat().st_size > 0:
-        print("[✓] Reusing gowitness database")
-        return
 
     print("[*] Running gowitness screenshot capture...")
 
@@ -50,8 +44,7 @@ def run_gowitness(url_file, output_dir):
         "file",
         "-f",
         url_file,
-        "--db-path",
-        str(db_path),
+        "--write-db",
         "--screenshot-path",
         str(screenshot_dir),
         "--threads",
@@ -69,33 +62,20 @@ def run_nuclei(url_file, output_file):
         "nuclei",
         "-l",
         url_file,
-
-        # limited important templates
         "-tags",
         "cves,misconfig,exposure,tech",
-
-        # severity filtering
         "-severity",
         "critical,high",
-
-        # disable interactsh noise
         "-no-interactsh",
-
-        # low noise settings
         "-rate-limit",
         "30",
-
         "-c",
         "5",
-
         "-retries",
         "1",
-
         "-timeout",
         "4",
-
         "-silent",
-
         "-o",
         output_file
     ]
