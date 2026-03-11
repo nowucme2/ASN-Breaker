55936a2dc83fa478b3ef10ee6d9f2ae1a08d19f7
AbhiCN <93698894+nowucme2@users.noreply.github.com>
2026-03-11 19:26:33 +0530

    Update project.py

--- module/project.py
+++ module/project.py
@@ -1,7 +1,39 @@
 from pathlib import Path
 
-def create_project(target):
-    base = Path("output") / target
+
+def select_or_create_project():
+
+    base = Path("output")
+    base.mkdir(exist_ok=True)
+
+    projects = [p.name for p in base.iterdir() if p.is_dir()]
+
+    if projects:
+        print("\nExisting Projects:\n")
+
+        for i, p in enumerate(projects, 1):
+            print(f"{i}. {p}")
+
+        print(f"{len(projects)+1}. Create New Project")
+
+        choice = input("\nSelect project number: ").strip()
+
+        try:
+            choice = int(choice)
+
+            if choice <= len(projects):
+                target = projects[choice-1]
+                print(f"\n[✓] Using existing project: {target}")
+            else:
+                target = input("Enter new target name: ").strip()
+
+        except:
+            target = input("Enter new target name: ").strip()
+
+    else:
+        target = input("Enter new target name: ").strip()
+
+    project_path = base / target
 
     dirs = [
         "ips",
@@ -13,6 +45,6 @@ def create_project(target):
     ]
 
     for d in dirs:
-        (base / d).mkdir(parents=True, exist_ok=True)
+        (project_path / d).mkdir(parents=True, exist_ok=True)
 
-    return base
+    return target, project_path
