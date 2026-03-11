cfadf741c9aa51f0ff38a8bbd43eb82f95e39c3f
AbhiCN <93698894+nowucme2@users.noreply.github.com>
2026-03-11 15:45:48 +0530

    Update asn_breaker.py

--- asn_breaker.py
+++ asn_breaker.py
@@ -5,10 +5,11 @@ from module.dependency_check import check_dependencies
 from module.bbot_parser import parse_bbot_table
 from module.subnet_intel import analyze_subnets
 from module.asn_lookup import get_asn_prefixes
+from module.project import init_project
 from module.scanner import run_scanner
 from module.web_scan import run_web_scan
 from module.report import generate_report
-from module.project import init_project
+from module.domain_filter import domain_filter
 
 
 def main():
@@ -19,13 +20,13 @@ def main():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-b", "--bbot")
-    parser.add_argument("-a", "--asn")
-    parser.add_argument("-c", "--cidr")
+    parser.add_argument("-b", "--bbot", help="BBOT ASN table")
+    parser.add_argument("-a", "--asn", help="ASN number")
+    parser.add_argument("-c", "--cidr", help="CIDR subnet")
 
     args = parser.parse_args()
 
-    project_dir, reuse_data = init_project()
+    project_dir, reuse = init_project()
 
     if args.bbot:
 
@@ -46,9 +47,12 @@ def main():
 
     subnets = analyze_subnets(raw_subnets)
 
-    http_file = run_scanner(subnets, project_dir, reuse_data)
+    ip_file = run_scanner(subnets, project_dir, reuse)
 
-    run_web_scan(http_file, project_dir)
+    # NEW DOMAIN FILTER
+    filtered_ips = domain_filter(ip_file, project_dir)
+
+    http_file = run_web_scan(filtered_ips, project_dir)
 
     generate_report(subnets, project_dir)
 
