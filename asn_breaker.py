b9fd505ec62c7285e566b986965e311a81f7c870
AbhiCN <93698894+nowucme2@users.noreply.github.com>
2026-03-11 11:32:14 +0530

    Update asn_breaker.py

--- asn_breaker.py
+++ asn_breaker.py
@@ -1,43 +1,49 @@
 import argparse
-from modules.subnet_parser import process_subnets
-from modules.scanner import run_scanner
-from modules.web_vuln_scan import run_web_scan
+
+from modules.banner import show_banner
+from modules.dependency_check import check_dependencies
+from modules.bbot_parser import parse_bbot_table
+from modules.subnet_intel import analyze_subnets
 from modules.asn_lookup import get_asn_prefixes
-from modules.report_generator import generate_report
+from modules.scanner import run_scanner
+from modules.web_scan import run_web_scan
+from modules.report import generate_report
 
 
 def main():
 
+    show_banner()
+
+    check_dependencies()
+
     parser = argparse.ArgumentParser(description="ASN Breaker")
 
-    parser.add_argument("-f", "--file", help="BBOT ASN table file")
+    parser.add_argument("-b", "--bbot", help="BBOT ASN table file")
     parser.add_argument("-a", "--asn", help="ASN number")
     parser.add_argument("-c", "--cidr", help="CIDR subnet")
 
     args = parser.parse_args()
 
-    subnets = []
+    raw_subnets = []
 
-    if args.file:
+    if args.bbot:
 
-        subnets = process_subnets(args.file)
-
-    elif args.cidr:
-
-        subnets = [{"subnet": args.cidr, "limit": 1000}]
+        raw_subnets = parse_bbot_table(args.bbot)
 
     elif args.asn:
 
-        prefixes = get_asn_prefixes(args.asn)
+        raw_subnets = get_asn_prefixes(args.asn)
 
-        for p in prefixes:
+    elif args.cidr:
 
-            subnets.append({"subnet": p, "limit": 1000})
+        raw_subnets = [args.cidr]
 
     else:
 
-        print("Provide -f OR -a OR -c")
+        print("Provide -b BBOT table OR -a ASN OR -c CIDR")
         return
+
+    subnets = analyze_subnets(raw_subnets)
 
     http_file = run_scanner(subnets)
 
@@ -45,7 +51,7 @@ def main():
 
     generate_report(subnets)
 
-    print("\nASN Breaker scan complete")
+    print("\nScan finished. Open output/report.html")
 
 
 if __name__ == "__main__":
